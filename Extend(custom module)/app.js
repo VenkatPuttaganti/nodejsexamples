edf2d11617220fd9e1a03451cdf362717402781b
Venkateswara Rao Puttaganti <venkateswara.puttaganti@northgateps.com>
2018-05-23 03:35:28 +0530

    Updated code.

--- Extend(custom module)/app.js
+++ Extend(custom module)/app.js
@@ -1,6 +1,6 @@
 var localTutor=require('./NodeTutorial.js');
 localTutor.NodeTutorial();
-//localTutor.NodeTutorial.pTutor();
+localTutor.NodeTutorial.pTutor();
 
 //Or use this code
 //var tut = new localTutor.NodeTutorial();  // Create and save object
